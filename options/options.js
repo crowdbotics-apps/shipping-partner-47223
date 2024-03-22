299a742896616781756670fb27cdbedfff494108
Crowdbotics <team@crowdbotics.com>
2024-03-22 10:42:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shipping-partner-47223",
+  url: "https://shipping-partner-47223.botics.co",
+  api: "https://shipping-partner-47223.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
